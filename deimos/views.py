48594e4a3583c492e1357d0df6f0b5c053e63e9e
arielfayol37 <arielfayol1@gmail.com>
2023-09-22 17:11:39 -0500

    rethought the logic of course_management display. Now an assigment is displayed only if an AssignmentStudent object for that assignment and student exists

--- deimos/views.py
+++ deimos/views.py
@@ -44,7 +44,9 @@ def course_management(request, course_id):
 
     if not is_enrolled:
         return HttpResponseForbidden('You are not enrolled in this course.')
-    assignments = Assignment.objects.filter(course=course, is_assigned=True)
+    assignments = Assignment.objects.filter(course=course,\
+                                             #is_assigned=True,\
+                                            assignmentstudent__student=student)
     context = {
         "assignments": assignments,
         "course": course
